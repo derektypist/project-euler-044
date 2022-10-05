ebdbf59a05fc818f22766bc93541fe95ff91418b
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-10-05 09:29:27 +0000

    Added pentagonNumbers function at script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,21 @@ function isPentagonal(n) {
     let root = (1+24*n) ** 0.5;
     return ((1+root)/6) % 1 == 0;
 }
+
+/*
+    Function to Return the minimum difference of two pentagonal
+    numbers Pi and Pj such that Pi + Pj is pentagonal and
+    Pj - Pi is pentagonal.
+    pentagonNumbers() returns 5482660.
+
+*/
+function pentagonNumbers() {
+    const PENTAGONALS = Array(10000).fill(0);
+    for (let i=1;i<10000;i++) {
+        const pI = PENTAGONALS[i] = i * (3*i-1)/2;
+        for (let j=i-1;j>0;j--) {
+            const pJ = PENTAGONALS[j];
+            if (isPentagonal(pI - pJ) && isPentagonal(pI + pJ)) return pI - pJ;
+        }
+    }
+}
